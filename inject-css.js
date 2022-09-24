5a12a0b35c99ae48bf9157196faadc18012defe5
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-09-24 17:23:21 +0200

    first commit

---
+++ inject-css.js
@@ -0,0 +1,40 @@
+var style = document.createElement("style");
+style.appendChild(document.createTextNode(`ytd-rich-section-renderer #content {
+    border-radius: 10px;
+    transition: all .1s;
+}
+ytd-rich-section-renderer #content:hover {
+    transform: scale(1.007);
+}
+
+ytd-compact-video-renderer {
+    border-radius: 10px;
+    transition: all .1s;
+}
+ytd-compact-video-renderer:hover {
+    box-shadow: 5px 5px 15px #000;
+    transform: scale(1.03);
+}
+
+ytd-compact-radio-renderer {
+    border-radius: 10px;
+    transition: all .1s;
+}
+ytd-compact-radio-renderer:hover {
+    box-shadow: 5px 5px 15px #000;
+    transform: scale(1.03);
+}
+
+
+ytd-rich-item-renderer #content {
+    border-radius: 10px;
+    transition: all .1s;
+}
+ytd-rich-item-renderer #content:hover {
+    box-shadow: 5px 5px 15px #000;
+    transform: scale(1.03);
+}`));
+document.body.appendChild(style);
+// style.sheet.insertRule(``, 0);
+console.log("CSS injected");
+
