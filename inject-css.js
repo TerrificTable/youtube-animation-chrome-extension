b094256b1d21bb1d3d205f7657e3fe365f844d5d
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-09-25 22:14:43 +0200

    cool rainbow subscribe button :)

--- inject-css.js
+++ inject-css.js
@@ -1,5 +1,67 @@
 var style = document.createElement("style");
 style.appendChild(document.createTextNode(`
+#subscribe-button {
+    padding: 0 15px ;
+}
+
+.yt-spec-button-shape-next--mono.yt-spec-button-shape-next--filled {
+    color: #fff;
+}
+
+.yt-spec-button-shape-next--filled {
+    border: none;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    width: 200px;
+    border-radius: 10px;
+}
+
+.yt-spec-button-shape-next--filled:after {
+    z-index: -1;
+    
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    
+    border-radius: 10px;
+}
+
+.yt-spec-button-shape-next--filled:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-size: 400%;
+    z-index: -1;
+    border-radius: 10px;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 1;
+    transition: opacity .3s ease-in-out;
+}
+
+
+@keyframes glowing {
+    0% {
+        background-position: 0 0;
+    }
+    50% {
+        background-position: 400% 0;
+    }
+    100% {
+        background-position: 0 0;
+    }
+}
+
+
 ytd-compact-link-renderer {
     border-radius: 10px;
     transition: all .1s;
