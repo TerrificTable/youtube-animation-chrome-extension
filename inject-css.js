2e0692150d1c62823130c8b7e20a80737cd3fa3a
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-09-25 23:43:20 +0200

    cool rainbow glow buttons for subscribed and not subscribed

--- inject-css.js
+++ inject-css.js
@@ -1,8 +1,49 @@
 var style = document.createElement("style");
 style.appendChild(document.createTextNode(`
 #subscribe-button {
-    padding: 0 15px ;
+    padding: 0 15px;
+    color: #fff;
 }
+
+#subscribe-button .yt-spec-button-shape-next--mono.yt-spec-button-shape-next--tonal {
+    border: none;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    width: 200px;
+    border-radius: 10px;
+}
+
+#subscribe-button .yt-spec-button-shape-next--mono.yt-spec-button-shape-next--tonal:after {
+    z-index: -1;
+    
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    
+    border-radius: 10px;
+}
+#subscribe-button .yt-spec-button-shape-next--mono.yt-spec-button-shape-next--tonal:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff00c8, #002bff, #00ffd5, #48ff00, #fffb00, #7a00ff, #ff7300, #ff0000);
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-size: 400%;
+    z-index: -1;
+    border-radius: 10px;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing-left 20s linear infinite;
+    opacity: 1;
+    transition: opacity .3s ease-in-out;
+}
+
 
 .yt-spec-button-shape-next--mono.yt-spec-button-shape-next--filled {
     color: #fff;
@@ -43,13 +84,13 @@ style.appendChild(document.createTextNode(`
     filter: blur(5px);
     width: calc(100% + 4px);
     height: calc(100% + 4px);
-    animation: glowing 20s linear infinite;
+    animation: glowing-right 20s linear infinite;
     opacity: 1;
     transition: opacity .3s ease-in-out;
 }
 
 
-@keyframes glowing {
+@keyframes glowing-left {
     0% {
         background-position: 0 0;
     }
@@ -60,6 +101,20 @@ style.appendChild(document.createTextNode(`
         background-position: 0 0;
     }
 }
+
+
+@keyframes glowing-right {
+    0% {
+        background-position: 0 0;
+    }
+    50% {
+        background-position: -400% 0;
+    }
+    100% {
+        background-position: 0 0;
+    }
+}
+
 
 
 ytd-compact-link-renderer {
